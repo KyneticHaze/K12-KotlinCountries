6cb260190727ed94ca6d3c0536e5151a3bd4834f
furkan harmancı <rocklee34@hotmail.com>
2023-10-04 14:58:23 +0300

    feat: dataBinding is added :sparkles:

--- app/src/main/java/com/example/kotlincountries/adapter/CountryAdapter.kt
+++ app/src/main/java/com/example/kotlincountries/adapter/CountryAdapter.kt
@@ -2,23 +2,28 @@ package com.example.kotlincountries.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlincountries.R
 import com.example.kotlincountries.databinding.ItemCountryBinding
 import com.example.kotlincountries.model.Country
-import com.example.kotlincountries.util.getImageWithGlide
-import com.example.kotlincountries.util.placeholderProgressBar
 import com.example.kotlincountries.view.CountriesFragmentDirections
 
-class CountryAdapter(private val countryList: ArrayList<Country>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+class CountryAdapter(private val countryList: ArrayList<Country>):
+    RecyclerView.Adapter<CountryAdapter.CountryViewHolder>(),
+        CountryClickListener
+{
 
     inner class CountryViewHolder(var view: ItemCountryBinding) : RecyclerView.ViewHolder(view.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
-        val view = ItemCountryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<ItemCountryBinding>(inflater, R.layout.item_country,parent,false)
         return CountryViewHolder(view)
     }
 
@@ -27,7 +32,8 @@ class CountryAdapter(private val countryList: ArrayList<Country>) : RecyclerView
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        holder.view.country.text = countryList[position].countryName
+        /*
+                holder.view.country.text = countryList[position].countryName
         holder.view.region.text = countryList[position].countryRegion
         holder.view.imageView.getImageWithGlide(countryList[position].imageUrl,
             placeholderProgressBar(holder.itemView.context)
@@ -37,6 +43,11 @@ class CountryAdapter(private val countryList: ArrayList<Country>) : RecyclerView
             val action = CountriesFragmentDirections.actionCountriesFragmentToShowCountryFragment(countryList[position].uuid)
             Navigation.findNavController(it).navigate(action)
         }
+         */
+
+        var country = holder.view.getCountry()
+        country = countryList[position]
+        holder.view.listener = this
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -44,5 +55,11 @@ class CountryAdapter(private val countryList: ArrayList<Country>) : RecyclerView
         countryList.clear()
         countryList.addAll(newCountryList)
         notifyDataSetChanged()
+    }
+
+    override fun onCountryClicked(v: View) {
+        val uuid = ItemCountryBinding.bind(v).countryUuidText.text.toString().toInt()
+        val action = CountriesFragmentDirections.actionCountriesFragmentToShowCountryFragment(uuid)
+        Navigation.findNavController(v).navigate(action)
     }
 }
--- app/src/main/java/com/example/kotlincountries/view/ShowCountryFragment.kt
+++ app/src/main/java/com/example/kotlincountries/view/ShowCountryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.kotlincountries.R
 import com.example.kotlincountries.databinding.FragmentShowCountryBinding
@@ -13,7 +14,7 @@ import com.example.kotlincountries.util.placeholderProgressBar
 import com.example.kotlincountries.viewModel.ShowCountryViewModel
 
 class ShowCountryFragment : Fragment() {
-    private lateinit var binding: FragmentShowCountryBinding
+    private lateinit var dataBinding: FragmentShowCountryBinding
     private var countryUUID = 0
     private lateinit var viewModel: ShowCountryViewModel
 
@@ -26,8 +27,8 @@ class ShowCountryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentShowCountryBinding.inflate(inflater, container, false)
-        return binding.root
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_show_country, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,24 +37,26 @@ class ShowCountryFragment : Fragment() {
         arguments?.let {
             countryUUID = ShowCountryFragmentArgs.fromBundle(it).uuid
         }
-
         viewModel = ViewModelProviders.of(this)[ShowCountryViewModel::class.java]
         viewModel.getDataFromRoom(countryUUID)
-
         observeLiveData()
     }
 
     private fun observeLiveData() {
         viewModel.countryLiveData.observe(viewLifecycleOwner) {country ->
             country?.let {
-                binding.countryName.text = country.countryName
-                binding.countryCapital.text = country.countryCapital
-                binding.countryRegion.text = country.countryRegion
-                binding.countryLanguage.text = country.countryLanguage
-                binding.countryCurrency.text = country.countryCurrency
+                dataBinding.selectedCountry = it
+
+                /*
+                dataBinding.countryName.text = country.countryName
+                dataBinding.countryCapital.text = country.countryCapital
+                dataBinding.countryRegion.text = country.countryRegion
+                dataBinding.countryLanguage.text = country.countryLanguage
+                dataBinding.countryCurrency.text = country.countryCurrency
                 context?.let {
-                    binding.countryImage.getImageWithGlide(country.imageUrl, placeholderProgressBar(it))
+                    dataBinding.countryImage.getImageWithGlide(country.imageUrl, placeholderProgressBar(it))
                 }
+                 */
             }
         }
     }
